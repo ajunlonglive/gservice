02ecab16ac8b2346bce9d27670bef6e9c39a84e0
黑哥 <81855841@qq.com>
2018-11-30 17:27:30 +0800

    w

--- gsoap/Clear.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-for /f "delims=" %%i in ('dir /a-d /b') do (
-	if /i "%%i" neq "xml-rpc.h" if /i "%%i" neq "xml-rpc.c" if /i "%%i" neq "json.h" if /i "%%i" neq "json.c" if /i "%%i" neq "stdsoap2.h" if /i "%%i" neq "stdsoap2.c" if /i "%%i" neq "xml-rpc-iters.h" if /i "%%i" neq "Server.bat"  if /i "%%i" neq "Clear.bat" (
-		del %%i
-	)
-)
--- gsoap/Server.bat
+++
@@ -1,2 +0,0 @@
-D:\library\gsoap\bin\win32\soapcpp2.exe -c -CSL xml-rpc.h
-pause
